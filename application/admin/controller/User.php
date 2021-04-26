aaeebce29ae42f17f5f60684c9252d1d195fb421
Smnatic95 <luolips@qq.com>
2021-04-26 17:41:17 +0800

    init

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\admin\controller;
 
 use think\Db;
@@ -9,31 +8,28 @@ use app\admin\model\User as userModel;
 
 class User extends BaseController
 {
-    public $userModel;
-
-    // 初始化方法
-    public function _initialize()
-    {
-        // $this->$userModel  = new userModel();
-    }
-
     public function login()
     {
-        return (new UserModel)->select1();
-        // if (request()->isPost()) {
-        //     $reqParms = request()->param();
-        //     $user = Db::name('admin_user')->where('name', $reqParms['name'])->find();
-        //     if (!$user) {
-        //         return $this->myApiRes(0, '用户不存在');
-        //     } elseif ($user['password'] !=  $reqParms['password']) {
-        //         return $this->myApiRes(0, '密码错误');
-        //     } else {
-        //         $jwt = $this->createToken($user['id']);
-        //         Db::name('admin_user')->where('id', $user['id'])->update(['jwt' => $jwt , 'lrtime' => date('Y-m-d H:i:s') ]);
-        //         return $this->myApiRes(1, $jwt);
-        //     }
-        // } else {
-        //     return $this->myApiRes(0, '请求方式错误');
-        // }
+        if (request()->isPost()) {
+            $reqParms = request()->param();
+            $user = Db::name('admin_user')->where('name', $reqParms['name'])->find();
+            if (!$user) {
+                return $this->myApiRes(0, '用户不存在');
+            }
+            if ($user['password'] != $reqParms['password']) {
+                return $this->myApiRes(0, '密码错误');
+            }
+                $jwt = $this->createToken($user['id']);
+                Db::name('admin_user')->where('id', $user['id'])->update(['jwt' => $jwt, 'lrtime' => date('Y-m-d H:i:s')]);
+                return $this->myApiRes(1, $jwt);
+        } else {
+            return $this->myApiRes(0, '请求方式错误');
+        }
     }
+
+    public function checkLogin(){
+        $res = userModel::checkLogin('tO2xzm_c2z7Mfv7Kk0XPTQz4Al41');
+        return $res;
+    }
+
 }
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -6,11 +6,24 @@ use think\Model;
 
 class User extends Model
 {
-    // 设置当前模型对应的完整数据表名称
+    private static $logoutTime = 1 * 24 * 60 * 60;
     protected $table = 'admin_user';
 
     public function select1()
     {
-        return User::get(1);
+        return self::get(1);
+    }
+
+    public static function checkLogin($jwt)
+    {
+        $cuser = User::get(['jwt' => $jwt]);
+        if (!$cuser) {
+            return json(['code' => 0, 'msg' => 'token错误']);
+        }
+        $timeCha = time() - strtotime($cuser->data['lrtime']);
+        if ($timeCha > User::$logoutTime) {
+            return json(['code' => 403, 'msg' => '登陆过期']);
+        }
+        return json(['code' => 1, 'data' => $cuser->data]);
     }
 }
